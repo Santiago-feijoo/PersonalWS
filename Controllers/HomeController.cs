e61f5ac3207088da307cbf3263ea0204ad8cb42b
Santiago Feijoo <49215448+Santiago-feijoo@users.noreply.github.com>
2022-06-28 23:39:27 -0500

    Importando codigo inicial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWS.Models;
+
+namespace PersonalWS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("GetDocumentTypes")]
+        public DocumentTypes getDocumentTypes()
+        {
+            var documentType = new DocumentTypes();
+            documentType.documentTypeId = 1;
+            documentType.acronym = "CC";
+            documentType.description = "Cedula de ciudadanía";
+
+            return documentType;
+        }
+
+    }
+}
---
+++ Models/DocumentTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalWS.Models
+{
+    public class DocumentTypes
+    {
+        public int documentTypeId { get; set; }
+        public string acronym { get; set; }
+        public string description { get; set; }
+    }
+}
